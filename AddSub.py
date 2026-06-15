19bd215c61ebf0fa7012ed2a8aa08b46a59b4001
kiransankhe399 <kiransankhe399@gmail.com>
2026-06-15 22:55:33 +0530

    Add files via upload

---
+++ AddSub.py
@@ -0,0 +1,18 @@
+print("----------------------------------------------------------")
+print("-------------Program to accept two numbers----------------")
+print("----------------------------------------------------------")
+
+num1 = int(input("Enter value for a:"))
+num2 = int(input("Enter value for b:"))
+
+add = num1 + num2
+sub = num1 - num2
+multi = num1 * num2
+div = num1 / num2
+
+print ("Addition is:", add)
+print ("Substraction is:", sub)
+print("Multiplication is :",multi)
+print("Division is :",div)
+
+print("----------------------------------------------------------")
